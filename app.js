f0aa175929fbc3ff58cc097aead788cc2514212a
Salma Rakha <salmarakha75@gmail.com>
2021-12-19 12:54:34 +0200

    connect to mysql and create db

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const express = require("express");
+const express = require('express');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
+// DB connection
+require('./config/connection');
+
 const PORT = 3000;
 
 app.use('/', routes);
+
+app.use((err, req, res, next) => {
+    // global error handler
+    if (err)
+        console.log(err);
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server is up and running on port: ${PORT}`);

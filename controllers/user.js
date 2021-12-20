43ce3efb4c1e73e89cb7cb242e6d377539134099
Salma Rakha <salmarakha75@gmail.com>
2021-12-20 15:20:24 +0200

    update and delete user

--- controllers/user.js
+++ controllers/user.js
@@ -11,7 +11,20 @@ const addUser = (userData) => {
     return User.create(userData);
 }
 
+const editUser = async (id, newData) => {
+    await User.update(newData, { where: { id: id }, returning: true });
+    return User.findByPk(id); // return after update
+}
+
+const removeUser = async (id) => {
+    const deletedUser = User.findByPk(id);
+    await User.destroy({ where: { id: id } });
+    return deletedUser;
+}
+
 module.exports = {
     getById,
-    addUser
+    addUser,
+    editUser,
+    removeUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 const express =require('express');
 const router = express.Router();
-const { getById, addUser } = require('../controllers/user');
+const { getById, addUser, editUser, removeUser } = require('../controllers/user');
 
 
 router.get('/:id', findById);
-router.post('/', createUser)
+router.post('/', createUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
 
 function findById (req, res, next) {
     const { id: userId } = req.params
@@ -20,6 +23,25 @@ function createUser (req, res, next) {
     addUser(userData)
     .then(result => {
         res.status(201).json(result)
+    })
+    .catch(err => next(err));
+}
+
+function updateUser (req, res, next) {
+    const { id } = req.params;
+    const { body: newData } = req;
+    editUser(id, newData)
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => next(err));
+}
+
+function deleteUser (req, res, next) {
+    const { id } = req.params;
+    removeUser(id)
+    .then(result => {
+        res.status(200).json(result);
     })
     .catch(err => next(err));
 }
